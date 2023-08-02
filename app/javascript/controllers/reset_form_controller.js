3f96ec67166093262dfaf9133bb49e44c2b3f7fe
Lucius Choi <lucius.choi@gmail.com>
2023-08-02 16:58:20 +0900

    디버그 로그 삭제

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -2,9 +2,6 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="reset-form"
 export default class extends Controller {
-  connect() {
-    console.log("Hello, Stimulus!", this.element);
-  }
   reset() {
     this.element.reset();
   }
